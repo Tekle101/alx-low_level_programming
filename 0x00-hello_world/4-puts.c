6e5ddac2b77585961971be9c2dd306eb29186933
Tekle101 <tekletekle2421@gmail.com>
2022-04-07 21:59:33 +0300

    puts .c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <>[Dstdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 ()[Dsuccess)
+ */
+int main (void)
+[D[A int main (void)
+ {
+         puts{""[D\"programing is like building a multilingual puzzle");
+         return (0);
+ }
+
